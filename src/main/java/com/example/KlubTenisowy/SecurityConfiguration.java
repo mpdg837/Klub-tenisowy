34c4ab30e993f4777bf0ef4d6148e83621a33887
JedrzejJoniec <72892446+JedrzejJoniec@users.noreply.github.com>
2022-01-31 01:28:21 +0100

    Add files via upload

---
+++ src/main/java/com/example/KlubTenisowy/SecurityConfiguration.java
@@ -0,0 +1,45 @@
+package com.example.KlubTenisowy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import
+org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+		.authorizeRequests()
+		.antMatchers("/", "/index","biura","pracownicy").permitAll()
+		.anyRequest().authenticated()
+		.and()
+		.formLogin()
+		.loginPage("/pracownicy")
+		.permitAll()
+		.and()
+		.logout()
+		.logoutSuccessUrl("/index")
+		.permitAll();
+	}
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+		.withUser("user")
+		.password("user")
+		.roles("USER")
+		.and()
+		.withUser("admin")
+		.password("admin")
+		.roles("ADMIN");
+}
+	@Bean
+	public PasswordEncoder getPasswordEncoder() { 
+		return NoOpPasswordEncoder.getInstance();
+	}
+}
